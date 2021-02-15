4a0011af02313df8230b8a27f020dbe3acb2b073
Christian <christian.bauer@mekra.de>
2021-02-15 15:31:55 +0100

    whitespaces

--- src/config.h
+++ src/config.h
@@ -7,20 +7,20 @@
 namespace config {
 
 	// Audio input settings
-	static const uint sampleSize = 16;		// 8 bit per sample
-	static const uint sampleRate = 44100;	// 44100 samples per second
+	static const uint sampleSize = 16;      // 8 bit per sample
+	static const uint sampleRate = 44100;   // 44100 samples per second
 
 	// Measure limits
 	static const double cutoffMag = 300;    // Cutoff magnitude
-	static const double cutoffLower = 200;	// Lower cutoff frequency
+	static const double cutoffLower = 200;  // Lower cutoff frequency
 	static const double cutoffUpper = 3000; // Upper cutoff frequency
 
 	// Frequencies and tolerances
-	static const double f1 = 1800;			// First freqency
-	static const double f2 = 1400;			// Second freqency
-	static const double pause = 300;		// Pause between two tones in ms
-	static const double deltaF = 150;		// Frequencies +- this is ok
-	static const double deltaT = 100;		// Pause +- this is ok
+	static const double f1 = 1800;          // First freqency
+	static const double f2 = 1400;          // Second freqency
+	static const double pause = 300;        // Pause between two tones in ms
+	static const double deltaF = 150;       // Frequencies +- this is ok
+	static const double deltaT = 100;       // Pause +- this is ok
 	static const QList<double> freqs = {
 		1800, 1400
 	};
